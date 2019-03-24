3289fd70c8dc076fced348f16ebd9967c711ef38
Gammy <preetsc27@gmail.com>
2019-03-24 21:05:11 +0530

    Video 3 - Added bodyParser support for json

--- basics/src/app.ts
+++ basics/src/app.ts
@@ -1,12 +1,24 @@
 import * as express from 'express'
 import { Request, Response } from 'express';
 import { requestMiddleware }  from './middleware/requestMiddleware'
+import * as bodyParser from 'body-parser'
+
 const PORT = 8080
 const app = express()
 app.use(requestMiddleware)
+app.use(bodyParser.json())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello YouTube")
+})
+
+app.post("/data", (req: Request, res: Response) => {
+    console.log(req.body)
+    const { name, age, cool } = req.body
+    console.log(`Name: ${name}`)
+    console.log(`Age: ${age}`)
+    console.log(`Cool: ${cool}`)
+    res.send("Hello DAta")
 })
 
 app.listen(PORT, () => {
